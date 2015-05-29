05d4f4a240923521aef7224f58424fa55f05cc32
Juan P. Daza P <juan.daza@er-technology.net>
2015-05-29 01:12:01 -0500

    frontend, fixing errors

--- app/src/main/java/net/ertechnology/mybuttons/MainActivity.java
+++ app/src/main/java/net/ertechnology/mybuttons/MainActivity.java
@@ -1,17 +1,77 @@
 package net.ertechnology.mybuttons;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSpotify = (Button) findViewById(R.id.button_spotify);
+        Button buttonScores = (Button) findViewById(R.id.button_scores);
+        Button buttonLibrary = (Button) findViewById(R.id.button_library);
+        Button buttonBuild = (Button) findViewById(R.id.button_build);
+        Button buttonReader = (Button) findViewById(R.id.button_reader);
+        Button buttonMyApp = (Button) findViewById(R.id.button_myapp);
+
+        buttonSpotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("class", "entra");
+                Toast.makeText(getApplicationContext(), R.string.msg_spotify, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        buttonScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.msg_scores, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.msg_library, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonBuild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.msg_build, Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.msg_reader, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonMyApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), R.string.msg_myapp, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
